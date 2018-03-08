1d2c0dbcba400f04beb307fbf8a2984cd82704bb
Alexandre Conde <alexandreconde@hotmail.com>
2018-03-08 12:07:17 +0000

    fix Year

--- isbnlib_bnf/_bnf.py
+++ isbnlib_bnf/_bnf.py
@@ -61,7 +61,7 @@ def parser_bnf(xml):
         recs['Publisher'] = recs['Publisher'].split('|')[0]
         authors = recs['Authors'].split('|')
         recs['Authors'] = [_clean_author(author) for author in authors]
-        recs['Year'] = ''.join(c for c in recs['Year'] if c.isdigit())
+        recs['Year'] = u(''.join(c for c in recs['Year'] if c.isdigit())[:4])
         recs['Title'] = _clean_title(recs['Title'])
         recs['Language'] = recs['Language'].split('|')[0]
     except IndexError:
