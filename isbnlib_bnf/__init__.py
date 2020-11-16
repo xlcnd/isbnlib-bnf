b0d0ec4006e3744128221c607e699b02eb0b636b
Alexandre Conde <alexandreconde@hotmail.com>
2020-11-16 11:34:40 +0000

    fix plugin's metadata

--- isbnlib_bnf/__init__.py
+++ isbnlib_bnf/__init__.py
@@ -5,6 +5,9 @@ from ._bnf import query
 
 __version__ = '1.0.2'
 
-__plugin_main_language__ = 'fr'
+__plugin_author__ = 'xlcnd'
+__plugin_description__ = 'Metadata for French books from catalogue.bnf.fr'
+__plugin_homepage__ = 'github.com/xlcnd/isbnlib-bnf'
+__plugin_service_language__ = 'fr'
 __plugin_service_name__ = 'bnf'
 __plugin_type__ = 'metadata'
